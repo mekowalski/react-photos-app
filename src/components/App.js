622ab70d124711aa4a5dc4395b7ac3603e02538f
Malind Kowalski <malind.nikulski@gmail.com>
2019-01-22 16:51:04 -0700

    refactor App to class-based

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import SearchBar from './SearchBar'
 
-//functional Component that displays
-const App = () => {
-  return (
-    <div className='ui container' style={{marginTop: '10px'}}>
-      <SearchBar />
-    </div>
-  )
+//turn App to a class-based Component
+class App extends React.Component {
+  //define callback()
+  onSearchSubmit(term) {
+    console.log(term)
+  }
+
+  render() {
+    return (
+      <div className='ui container' style={{marginTop: '10px'}}>
+      //pass down callback() as a prop to SearchBar
+      //keep naming similar to function
+      <SearchBar onSubmit={this.onSearchSubmit}/>
+      </div>
+    );
+  }
 }
 
 export default App
