729552238887fe3d26d423a7e3478cdc525ac7f3
Malind Kowalski <malind.nikulski@gmail.com>
2019-01-24 15:44:24 -0700

    successfully add key to img tag and resolve issue

--- src/components/PhotoList.js
+++ src/components/PhotoList.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 const PhotoList = props => {
   const photos = props.photos.map((photo) => {
-    return <img src={photo.urls.regular} />
+    return <img key={photo.id} src={photo.urls.regular} />
     {
-      //find root JSX tag that is being returned
+      //find root JSX tag(<img>, it could be <div>) that is being returned from map statement(the img)
+      //give img a key= prop
+      //id is given from API in the photo object
     }
   });
     return <div>{photos}</div>;
