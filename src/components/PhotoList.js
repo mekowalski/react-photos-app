bc740f35d3465e2af9fbcb7b2f8d631d0bf0ef9f
Malind Kowalski <malind.nikulski@gmail.com>
2019-01-24 15:53:44 -0700

    remove comments

--- src/components/PhotoList.js
+++ src/components/PhotoList.js
@@ -2,12 +2,7 @@ import React from 'react';
 
 const PhotoList = props => {
   const photos = props.photos.map(({ description, id, urls }) => {
-    return <img alt={description} key={id} src={regular} />
-    {
-      //photo is referenced 3 times(L5)
-      //destructure out the 3 properties inside the inner function(description, id and urls)
-      //L4: holy moly!!!! i know what this is now and how it works!!!!
-    }
+    return <img alt={description} key={id} src={urls.regular} />
   });
     return <div>{photos}</div>;
 };
