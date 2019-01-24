7a95fadf2b783e431e94c86c25c219f45845f80f
Malind Kowalski <malind.nikulski@gmail.com>
2019-01-24 13:35:49 -0700

    remove comments, modify onSearchSubmit still with async

--- src/components/App.js
+++ src/components/App.js
@@ -4,10 +4,10 @@ import SearchBar from './SearchBar';
 
 class App extends React.Component {
   state = { photos: [] };
-  //expect state property to contain an array, allowing to call certain functions
-  //default it to an empty array
 
-  async onSearchSubmit(term) {
+  //Solve TypeError issue with ES6 arrow function
+  //define the =>(), mark it as async and assign it to an instance property
+  onSearchSubmit = async term => {
     const response = await axios
     .get('https://api.unsplash.com/search/photos', {
       params: { query: term },
@@ -15,7 +15,6 @@ class App extends React.Component {
         Authorization: 'Client-ID cd676e1ef029ce061d3722b47bc05267297569b249b49306547aa9fb1de2213e'
       }
     });
-    console.log(this)
     this.setState({ photos: response.data.results });
   }
 
@@ -24,10 +23,6 @@ class App extends React.Component {
       <div className='ui container' style={{marginTop: '10px'}}>
         <SearchBar onSubmit={this.onSearchSubmit}/>
         Found: {this.state.photos.length} photos
-        {
-          //Unhandled Rejection (TypeError): this.setState is not a function
-          //Use 1 of 3 solutions of: ES6 arrow function, constructor function or wrap onSearchSubmit
-        }
       </div>
     );
   }
