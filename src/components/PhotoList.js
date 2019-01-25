79f020891804b15a3a6b6e7864dcdffce5fa87f1
Malind Kowalski <malind.nikulski@gmail.com>
2019-01-25 15:15:51 -0700

    add css file and photos display in grid form, note perfect yet

--- src/components/PhotoList.js
+++ src/components/PhotoList.js
@@ -1,10 +1,11 @@
+import './PhotoList.css';
 import React from 'react';
 
 const PhotoList = props => {
   const photos = props.photos.map(({ description, id, urls }) => {
     return <img alt={description} key={id} src={urls.regular} />
   });
-    return <div>{photos}</div>;
+    return <div className='photo-list'>{photos}</div>;
 };
 
 export default PhotoList
