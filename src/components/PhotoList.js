27c5099e5376ba32dd665b7e6f26731c3098042a
Malind Kowalski <malind.nikulski@gmail.com>
2019-01-28 11:29:35 -0700

    explain the replacing of img with PhotoCard

--- src/components/PhotoList.js
+++ src/components/PhotoList.js
@@ -1,9 +1,18 @@
 import './PhotoList.css';
 import React from 'react';
+import PhotoCard form './PhotoCard';
 
 const PhotoList = props => {
-  const photos = props.photos.map(({ description, id, urls }) => {
-    return <img alt={description} key={id} src={urls.regular} />
+  {
+    //1. import PhotoCard
+    //2. Replace img with PhotoCard
+    //3. Remove alt, src
+    //4. No longer need destructuring, return to original form of recieving photo object
+    //5. Then pass that photo in as a prop
+    //6. Still need to provide a key
+  }
+  const photos = props.photos.map((photo) => {
+    return <PhotoCard key={photo.id} photo={photo} />
   });
     return <div className='photo-list'>{photos}</div>;
 };
