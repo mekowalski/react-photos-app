89b473b2c9a33ec9bf073564dba0186aa0ca213c
Malind Kowalski <malind.nikulski@gmail.com>
2019-01-29 16:35:53 -0700

    remove comments

--- src/components/PhotoList.js
+++ src/components/PhotoList.js
@@ -3,14 +3,6 @@ import React from 'react';
 import PhotoCard from './PhotoCard';
 
 const PhotoList = props => {
-  {
-    //1. import PhotoCard
-    //2. Replace img with PhotoCard
-    //3. Remove alt, src
-    //4. No longer need destructuring, return to original form of recieving photo object
-    //5. Then pass that photo in as a prop
-    //6. Still need to provide a key
-  }
   const photos = props.photos.map((photo) => {
     return <PhotoCard key={photo.id} photo={photo} />
   });
