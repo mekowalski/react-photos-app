728411314dbc33530380673472652f233c66d9fc
Malind Kowalski <malind.nikulski@gmail.com>
2019-01-29 16:56:06 -0700

    create method to determine span value, add event listener

--- src/components/PhotoCard.js
+++ src/components/PhotoCard.js
@@ -11,16 +11,17 @@ class PhotoCard extends React.Component {
   }
 
   //define componentDidMount()
+  //access the height ONLY after it becomes available
+  //add a basic, plain, vanilla, js event listener
+  //2nd arg will be a cb()
   componentDidMount() {
-    console.log(this.photoRef);
+    this.photoRef.current.addEventListener('load', this.setSpans);
+  }
+
+  //the ultimate prop inside is the 'grid-row-end' property that takes units of spans
+  //when the height is retrieved it will be used to set the span value
+  setSpans = () => {
     console.log(this.photoRef.current.clientHeight);
-    //chrome console is extremely 'fancy'
-    //when ref is printed out, console does not yet know what data is inside the img
-    //the console only knows the height once the img object is expanded and current: img is expanded
-    //THEN chrome looks at that DOM node, pulls info and prints it on the console
-    //when component first renders, the next second, the height of the image is printed out
-    //although the img has not yet loaded, therefore there is NO image
-    //this is about an order of operations
   }
 
   render() {
@@ -28,9 +29,7 @@ class PhotoCard extends React.Component {
     return (
       <div>
         <img ref={this.photoRef} alt={description} src={urls.regular} />
-      </div> //ref will tell a bit about the img DOM node, ref is a JSX tag, not a DOM element
-      //ref itself is a JS object that has a curent property referencing the DOM node
-      //in console can see the img tag and its clientHeight
+      </div>
     );
   }
 }
